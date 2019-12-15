ff02b3cc8f551c67af930ffeeb600d5762bcc40f
Jack Mcleans <jackagwaya@gmail.com>
2019-12-15 18:38:49 +0300

    Smart pointer implementation

--- DAY1/smart_pointers/unique_pointer/main.cpp
+++ DAY1/smart_pointers/unique_pointer/main.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
 #include <memory>
 
+class Person{
+    private:
+        std::string name;
+        std::string gender;
+        int age;
+    public:
+        Person(std::string name,
+                std::string gender,
+                int age): name(name),
+                            gender(gender),
+                            age(age){
+
+                            }
+        std::string  get_name(){
+            return this->name;
+        }
+
+        std::string get_gender(){
+            return this->gender;
+        }
+        int get_age(){
+            return this->age;
+        }
+
+        ~Person(){
+            std::cout<<"Object out of scope"<<std::endl;
+        }
+
+};
+
 int main(){
 
+    // a unique pointer can only be assigned to one object
+    //memory is allocated dynamically
+    //The memory is automatically freed when the object goes out of scope
 
-    std::cout<<"hello world"<<std::endl;
+    std::unique_ptr<Person> person = std::make_unique<Person>("Mcleans","Male",24);
+
+    std::cout<<"Details => "<<person->get_name()<<" "<<person->get_gender()<<" "<<person->get_age()<<std::endl;
+    
+
+
+
+
     return 0;
 }
