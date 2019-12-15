e92e1a0a75bb9f131caa9248002a095fd88d406e
Jack Mcleans <jackagwaya@gmail.com>
2019-12-15 16:28:18 +0300

    Implementation of student class

--- DAY1/inherritance/main.cpp
+++ DAY1/inherritance/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "staff.hpp"
+#include "student.hpp"
 
 int main(int argc , char *argv[]){
     Staff lecturer("James",
@@ -11,10 +12,27 @@ int main(int argc , char *argv[]){
                     "Engineering",
                     340000,
                     false);
+
+    Student first_year("Timothy",
+                        "Samuel",
+                        23,
+                        "male",
+                        "South B",
+                        "EEEQ/00921/2015",
+                        "Engineering",
+                        23000,
+                        false
+                        );
+
     lecturer.set_name("Jack","Mcleans");
     lecturer.set_car_pass_issued(true);
 
+    first_year.set_residence("Roysambu");
+    first_year.set_is_registered_for_exam(true);
+
+
    std::cout<<lecturer.get_name()<<std::endl;
+   std::cout<<first_year.get_residence()<<std::endl;
 
     return 0;
 }
