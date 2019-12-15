235928e3b948031832be8d23c00b82362aaf6687
Jack Mcleans <jackagwaya@gmail.com>
2019-12-15 14:04:00 +0300

    Staff class implementation

--- DAY1/inherritance/main.cpp
+++ DAY1/inherritance/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-
+#include "staff.hpp"
 
 int main(int argc , char *argv[]){
+    Staff lecturer("James",
+                    "kamau",
+                    34,
+                    "male",
+                    "Kasarani",
+                    "EMP/345",
+                    "Engineering",
+                    340000,
+                    false);
+    lecturer.set_name("Jack","Mcleans");
+    lecturer.set_car_pass_issued(true);
 
+   std::cout<<lecturer.get_name()<<std::endl;
 
     return 0;
 }
