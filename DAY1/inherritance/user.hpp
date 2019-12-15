ae1a4c73d8fc4a433b2b859b5f0453af039f555d
Jack Mcleans <jackagwaya@gmail.com>
2019-12-15 12:33:42 +0300

    user class header file declarations

---
+++ DAY1/inherritance/user.hpp
@@ -0,0 +1,35 @@
+#ifndef _USER_HPP
+#define _USER_HPP 
+
+#include <iostream>
+
+class User{
+    private:
+    private:
+
+    public:
+        std::string first_name;
+        std::string last_name;
+        int age;
+        std::string gender;
+        std::string residence;
+
+    public:
+        User(std::string first_name,
+                std::string last_name,
+                int age,
+                std::string gender,
+                std::string residence
+                );
+        std::string get_name();
+        void set_name(std::string first_name,
+                            std::string last_name);
+        int get_age():
+        void set_age(int age);
+
+        std::string get_residence();
+        void set_residence(std::string residence);
+};
+
+
+#endif //_USER_HPP
