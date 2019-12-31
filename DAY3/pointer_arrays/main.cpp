bd69fd749ff769fad2783a036a784f957c990f4f
Jack Mcleans <jackagwaya@gmail.com>
2019-12-31 14:12:14 +0300

    Pointer array implementation

--- DAY3/pointer_arrays/main.cpp
+++ DAY3/pointer_arrays/main.cpp
@@ -5,5 +5,12 @@ int main(int argc , char ** argv){
 
     std::cout<<"Hello world"<<std::endl;
 
+    std::string names[] = {"jack","james","simon","jane"};
+
+    std::string *current_prefect = names;
+
+    std::cout<<"current prefect = "<< names[3]<<std::endl;
+
+
     return 0;
 }
