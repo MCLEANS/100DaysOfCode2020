f1cdeed5445bc269f4cfadd647deb72409351a42
Jack Mcleans <jackagwaya@gmail.com>
2020-01-08 21:34:41 +0300

    syntax correction

--- DAY5/STL/VECTORS/main.cpp
+++ DAY5/STL/VECTORS/main.cpp
@@ -3,6 +3,14 @@
 
 int main(int argc , char *argv[]){
 
+    std::vector<int> my_vect;
+    //push first value to vector
+    my_vect.push_back(4);
+    //push second value to vector
+    my_vect.push_back(7);
+    //push third value to vector
+    my_vect.push_back(4);
+
 
     return 0;
 }
