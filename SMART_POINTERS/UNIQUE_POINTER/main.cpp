02d722c2b238bd2f4a3499266d9c125de8ebb312
MCLEANS <jackagwaya@gmail.com>
2020-04-24 20:38:55 +0300

    Unique pointer implementation

--- SMART_POINTERS/UNIQUE_POINTER/main.cpp
+++ SMART_POINTERS/UNIQUE_POINTER/main.cpp
@@ -18,11 +18,31 @@ class Student{
             std::cout<<"Student destroyed"<<std::endl;
         }
 
+        void revoke_suspension(){
+            this->on_suspension = false;
+        }
+
+        void suspend(){
+            this->on_suspension = true;
+        }
+        bool confirm_suspension()const{
+            return this->on_suspension;
+        }
+
 };
 
 int main(int argc, char **argv){
 
-    std::cout<<"HELLO WORLD"<<std::endl;
+   {
+       std::unique_ptr<Student> mcleans = std::make_unique<Student>(true,"Mathematics");
+       std::cout<<mcleans->confirm_suspension()<<std::endl;
+       mcleans->revoke_suspension();
+       std::cout<<mcleans->confirm_suspension()<<std::endl;
+
+
+       std::cout<<"Doing somathing before going out of scope"<<std::endl;
+
+   }
 
     return 0;
 }
