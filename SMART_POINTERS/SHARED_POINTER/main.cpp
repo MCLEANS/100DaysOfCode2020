f084fbf42b59e79590784e64eabb0a5e99a47cc2
MCLEANS <jackagwaya@gmail.com>
2020-04-24 21:10:12 +0300

    Student class creation

--- SMART_POINTERS/SHARED_POINTER/main.cpp
+++ SMART_POINTERS/SHARED_POINTER/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+class Student{
+    private:
+        bool is_suspended;
+        std::string course;
+    private:
+    public:
+    public:
+        Student(bool is_suspended,
+                std::string course):is_suspended(is_suspended),
+                                    course(course){
+
+            std::cout<<"Student created"<<std::endl;
+                                
+                                }
+
+        ~Student(){
+            std::cout<<"Student destroyed"<<std::endl;
+        }
+
+        void revoke_suspension(){
+            this->is_suspended = false;
+        }
+
+        void suspend(){
+            this->is_suspended = true;
+        }
+
+        bool confirm_suspension()const{
+            return this->is_suspended;
+        }
+};
+
+
 int main(int argc, char *argv[]){
-    std::cout<<"HELLO WORLD"<<std::endl;
+   
     return 0;
 }
