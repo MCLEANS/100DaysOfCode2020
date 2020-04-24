740f96f2dc0b1af46281b64fbdea8d2f347e81c6
MCLEANS <jackagwaya@gmail.com>
2020-04-24 20:03:36 +0300

    creation of student class

--- SMART_POINTERS/UNIQUE_POINTER/main.cpp
+++ SMART_POINTERS/UNIQUE_POINTER/main.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
+#include <memory>
+
+class Student{
+    private:
+        bool on_suspension;
+        std::string course;
+    private:
+    public:
+    public:
+        Student(bool on_suspension,
+                std::string course):on_suspension(on_suspension),
+                                    course(course){
+                                        std::cout<<"student created"<<std::endl;
+                                    }
+
+        ~Student(){
+            std::cout<<"Student destroyed"<<std::endl;
+        }
+
+};
 
 int main(int argc, char **argv){
 
