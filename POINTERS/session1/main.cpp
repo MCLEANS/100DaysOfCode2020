63d50e168ff8db5c1f4e5b90a5a94d9bf5212eaa
MCLEANS <jackagwaya@gmail.com>
2020-04-26 19:36:02 +0300

    pointer dereferencing

--- POINTERS/session1/main.cpp
+++ POINTERS/session1/main.cpp
@@ -9,5 +9,11 @@ int main(int argc, char *argv[]){
 
    std::cout<<"value is stored in memory address: "<<pvalue<<std::endl;
    std::cout<<"Size of a pointer is: "<<sizeof(pvalue)<<std::endl;
+
+   //pointer dereferencing
+   std::cout<<"The value stored in the specific memory address is: "<<*pvalue<<std::endl;
+   //with a pointer you have access to the memory cell and can use it to change memory content.
+   *pvalue = 60;
+   std::cout<<"int value has changed to: "<<value<<std::endl;
     return 0;
 }
