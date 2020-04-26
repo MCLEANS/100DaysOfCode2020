99e7f3f4790bc41abeac8d43bb449a1ec8df2606
MCLEANS <jackagwaya@gmail.com>
2020-04-26 20:02:43 +0300

    determining total number of elements in array

--- POINTERS/session2/main.cpp
+++ POINTERS/session2/main.cpp
@@ -2,5 +2,9 @@
 
 int main(int argc, char **argv){
 
+    std::string names[] = {"jack","jane","james"};
+
+    size_t elements = sizeof(names)/sizeof(std::string);
+    std::cout<<"Total number of elements in the array: "<<elements<<std::endl;
     return 0;
 }
