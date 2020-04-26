947207860a3c3753df4036e391f7754954ded74d
MCLEANS <jackagwaya@gmail.com>
2020-04-26 20:06:28 +0300

    first looping method

--- POINTERS/session2/main.cpp
+++ POINTERS/session2/main.cpp
@@ -6,5 +6,13 @@ int main(int argc, char **argv){
 
     size_t elements = sizeof(names)/sizeof(std::string);
     std::cout<<"Total number of elements in the array: "<<elements<<std::endl;
+
+    //LOOPING THROUGH ARRAY
+    //METHOD 1
+
+    for(int i=0;i<elements;i++){
+        std::cout<<names[i]<<" "<<std::flush;
+    }
+    std::cout<<"\n----------------------------------------\n";
     return 0;
 }
