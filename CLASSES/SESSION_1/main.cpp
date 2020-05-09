eb12e4608750467bdedff0cae712cd664529869d
MCLEANS <jackagwaya@gmail.com>
2020-05-09 17:28:54 +0300

    implementation of class inheritance

--- CLASSES/SESSION_1/main.cpp
+++ CLASSES/SESSION_1/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
-#include "person.h"
+#include "student.h"
 
 int main(int agrc, char * argv[]){
-    Person head_teacher("Jack","Mcleans",24,MALE);
+    
+    Student first_student;
+    first_student.set_first_name("Jack");
+    std::cout<<"First Name :"<<first_student.get_first_name()<<std::endl;
+
+    Student second_student("EEEQ/009702/2014",
+                            "ELECTRICAL AND ELECTRONICS",
+                            "ELECTRONICS AND COMPUTER",
+                            "COMPPUTER ENGINEERING",
+                            "JACK",
+                            "MCLEANS",
+                            24,
+                            MALE);
+
+    std::cout<<"DEPARTMENT : "<<second_student.get_department()<<std::endl;
+    
    
 
     return 0;
