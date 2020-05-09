a1d306e12038d79dca271a1fa22054c113731db3
MCLEANS <jackagwaya@gmail.com>
2020-05-09 09:27:30 +0300

    overloaded constructor

--- CLASSES/SESSION_1/person.h
+++ CLASSES/SESSION_1/person.h
@@ -20,6 +20,10 @@ class Person{
 
     public:
         Person();
+        Person(std::string first_name,
+                std::string last_name,
+                int age,
+                Gender gender);
         ~Person();
         void set_first_name(std::string first_name);
         std::string get_first_name()const;
